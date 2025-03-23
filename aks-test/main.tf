3236366c29267fd6969c3802e8ca1799b006d8cd
kp3073 <keyurpatel8288@gmail.com>
2025-03-23 15:29:41 -0400

    githubaction - asd

--- aks-test/main.tf
+++ aks-test/main.tf
@@ -1,26 +1,10 @@
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "${var.env}-aks"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  dns_prefix          = "cloudaws"
+module "aks" {
+  source = "./modules/aks"
+  env = var.env
+}
 
-  default_node_pool {
-	name           = "nodepool1"
-	node_count     = 3
-	vm_size        = "Standard_D2_v2"
-	vnet_subnet_id = data.azurerm_subnet.subnet.id
-  }
-
-  identity {
-	type = "SystemAssigned"
-  }
-  network_profile {
-	network_plugin     = "azure"
-	service_cidr = "10.3.0.0/16"
-	dns_service_ip = "10.3.0.10"
-  }
-
-  tags = {
-	Environment = "Production"
-  }
+module "vms" {
+  source = "./modules/vm"
+  vms = var.vms
+  env = dev
 }
---
+++ aks-test/modules/aks/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.env}-aks"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  dns_prefix          = "cloudaws"
+
+  default_node_pool {
+	name           = "nodepool1"
+	node_count     = 3
+	vm_size        = "Standard_D2_v2"
+	vnet_subnet_id = data.azurerm_subnet.subnet.id
+  }
+
+  identity {
+	type = "SystemAssigned"
+  }
+  network_profile {
+	network_plugin     = "azure"
+	service_cidr = "10.3.0.0/16"
+	dns_service_ip = "10.3.0.10"
+  }
+
+  tags = {
+	Environment = "Production"
+  }
+}
