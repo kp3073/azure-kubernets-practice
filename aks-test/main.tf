e3896353ffb3a5162e505c12275037170c68d707
kp3073 <keyurpatel8288@gmail.com>
2025-03-23 14:58:52 -0400

    githubaction - asd

--- aks-test/main.tf
+++ aks-test/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
+  name                = "${var.env}-aks"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  dns_prefix          = "exampleaks1"
+  dns_prefix          = "cloudaws"
 
   default_node_pool {
-	name           = "default"
-	node_count     = 1
+	name           = "nodepool1"
+	node_count     = 3
 	vm_size        = "Standard_D2_v2"
 	vnet_subnet_id = data.azurerm_subnet.subnet.id
   }
