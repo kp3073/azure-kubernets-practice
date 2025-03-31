593fc3bff655a160a28be344f8376131a4635054
kp3073 <keyurpatel8288@gmail.com>
2025-03-30 22:13:16 -0400

    githubaction -frontend

--- aks-test/main.tf
+++ aks-test/main.tf
@@ -6,6 +6,6 @@ module "vms" {
   for_each = var.vms  # var.component is a map for all the components , if you want to deploy app with Terraform
   source      = "./modules/vm"
   vms         = each.value["name"]
-  vm_size     = "Standard_B2s"
+  vm_size     = each.value["vm_size"]
   env         = var.env
 }
