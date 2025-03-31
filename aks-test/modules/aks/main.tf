f04e3f40c2a021ef29930d29f104d5a66432b4e7
kp3073 <keyurpatel8288@gmail.com>
2025-03-30 21:31:15 -0400

    githubaction -frontend

--- aks-test/modules/aks/main.tf
+++ aks-test/modules/aks/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_kubernetes_cluster" "example" {
 
   default_node_pool {
 	name           = "nodepool1"
-	node_count     = 3
+	node_count     = 2
 	vm_size        = "Standard_D2_v2"
 	vnet_subnet_id = data.azurerm_subnet.subnet.id
   }
