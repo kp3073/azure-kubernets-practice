e335fcb015f46dcf5d772b002fd348ec2bca1341
kp3073 <keyurpatel8288@gmail.com>
2025-03-30 21:26:54 -0400

    githubaction -frontend

--- aks-test/main.tf
+++ aks-test/main.tf
@@ -1,11 +1,11 @@
-# module "aks" {
-#   source = "./modules/aks"
-#   env = var.env
-# }
-module "vms" {
-  for_each = var.vms  # var.component is a map for all the components , if you want to deploy app with Terraform
-  source      = "./modules/vm"
-  vms         = each.value["name"]
-  vm_size     = "Standard_B2s"
-  env         = var.env
+module "aks" {
+  source = "./modules/aks"
+  env = var.env
 }
+# module "vms" {
+#   for_each = var.vms  # var.component is a map for all the components , if you want to deploy app with Terraform
+#   source      = "./modules/vm"
+#   vms         = each.value["name"]
+#   vm_size     = "Standard_B2s"
+#   env         = var.env
+# }
