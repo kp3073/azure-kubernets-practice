ec79fbe102f55df1ce096495a22679e4c4976cc1
kp3073 <keyurpatel8288@gmail.com>
2025-03-31 19:52:37 -0400

    githubaction -frontend

--- aks-test/modules/vms/main.tf
+++ aks-test/modules/vms/main.tf
@@ -18,9 +18,10 @@ resource "azurerm_network_interface" "main" {
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-	name                          = "testconfiguration1"
+	name                          = "internal"
 	subnet_id                     = data.azurerm_subnet.subnet.id
 	private_ip_address_allocation = "Dynamic"
+	public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
 
