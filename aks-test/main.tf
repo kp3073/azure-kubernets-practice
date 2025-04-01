eff82336edbea871a07a178a8f373d3cb8574b18
kp3073 <keyurpatel8288@gmail.com>
2025-04-01 15:05:03 -0400

    githubaction -frontend

--- aks-test/main.tf
+++ aks-test/main.tf
@@ -1,12 +1,12 @@
-# module "aks" {
-#   source = "./modules/aks"
-#   env = var.env
-# }
-
-
-module "vms" {
-  for_each = var.vms
-  source = "./modules/vms"
-  vms = var.vms
+module "aks" {
+  source = "./modules/aks"
   env = var.env
 }
+
+
+# module "vms" {
+#   for_each = var.vms
+#   source = "./modules/vms"
+#   vms = var.vms
+#   env = var.env
+# }
